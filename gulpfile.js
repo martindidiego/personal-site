0513a34d2c6636f3e2684b09737317ff698b80e9
Martin Di Diego <didiegomartin@gmail.com>
2018-06-10 16:57:28 -0400

    Finalized build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 // Gulp plugins
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var browserSync = require("browser-sync").create();
-var useref = require("gulp-useref");
-var uglify = require("gulp-uglify");
-var gulpIf = require("gulp-if");
-var cssnano = require("gulp-cssnano");
-var imagemin = require("gulp-imagemin");
-var cache = require("gulp-cache");
-var del = require("del");
-var runSequence = require("run-sequence");
-var autoprefixer = require("gulp-autoprefixer");
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+const uglify = require("gulp-uglify");
+const cssnano = require("gulp-cssnano");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
+const runSequence = require("run-sequence");
+const autoprefixer = require("gulp-autoprefixer");
+const babel = require("gulp-babel");
+const rename = require("gulp-rename");
+const concat = require("gulp-concat");
+const htmlreplace = require("gulp-html-replace");
 
 // Build config
 const config = {
@@ -21,7 +22,7 @@ const config = {
 };
 
 // SASS processing
-gulp.task("sass", function() {
+gulp.task("sass", () => {
   return gulp
     .src(`${config.paths.src}/stylesheets/scss/main.sass`)
     .pipe(sass())
@@ -34,56 +35,38 @@ gulp.task("sass", function() {
 });
 
 // Watch for SASS, HTML, JS file changes
-gulp.task("watch", ["browserSync", "sass"], function() {
+gulp.task("watch", ["browserSync", "sass"], () => {
   gulp.watch(`${config.paths.src}/stylesheets/scss/**/*.sass`, ["sass"]);
   gulp.watch(`${config.paths.src}/*.html`, browserSync.reload);
   gulp.watch(`${config.paths.src}/js/**/*.js`, browserSync.reload);
 });
 
 // Live reload via Browser Sync
-gulp.task("browserSync", function() {
+gulp.task("browserSync", () => {
   browserSync.init({ server: { baseDir: config.paths.src } });
 });
 
-// Concatenate JS and CSS files
-gulp.task("useref", function() {
-  return (
-    gulp
-      .src(`${config.paths.src}/*.html`)
-      .pipe(useref())
-      // Minifies JS and CSS files
-      .pipe(gulpIf("*.js", uglify()))
-      .pipe(gulpIf("*.css", cssnano()))
-      .pipe(gulp.dest(config.paths.dist))
-  );
+// Optimize images for production
+gulp.task("compress-images", () => {
+  return gulp
+    .src(`${config.paths.src}/images/**/*.+(png|jpg|jpeg|gif|svg|ico)`)
+    .pipe(
+      imagemin({
+        interlaced: true
+      })
+    )
+    .pipe(gulp.dest(`${config.paths.dist}/images`));
 });
 
-// Optimize images
-gulp.task("images", function() {
-  return (
-    gulp
-      .src(`${config.paths.src}/img/**/*.+(png|jpg|jpeg|gif|svg|ico)`)
-      // Cache images that already ran through imagemin
-      .pipe(
-        cache(
-          imagemin({
-            interlaced: true
-          })
-        )
-      )
-      .pipe(gulp.dest(`${config.paths.dist}/img`))
-  );
-});
-
-// Add fonts to dist
-gulp.task("fonts", function() {
+// Move fonts into dist for production
+gulp.task("move-fonts", () => {
   return gulp
     .src(`${config.paths.src}/fonts/**/*`)
     .pipe(gulp.dest(`${config.paths.dist}/fonts`));
 });
 
 // Add vendor prefixes to CSS files
-gulp.task("prefix", function() {
+gulp.task("prefix", () => {
   return gulp
     .src(config.paths.src + "/stylesheets/css/**/*.css")
     .pipe(
@@ -96,21 +79,72 @@ gulp.task("prefix", function() {
 });
 
 // Clean dist
-gulp.task("clean:dist", function() {
+gulp.task("clean:dist", () => {
   return del.sync(config.paths.dist);
 });
 
-// Clean dist + run Gulp tasks
-gulp.task("build", function(callback) {
-  runSequence(
-    "clean:dist",
-    "prefix",
-    ["sass", "useref", "images", "fonts"],
-    callback
-  );
+// Transpile all scripts into dist
+gulp.task("babel", () => {
+  gulp
+    .src(`${config.paths.src}/scripts/**/*.js`)
+    .pipe(babel({ presets: ["env"] }))
+    .pipe(gulp.dest(config.paths.dist));
+});
+
+// Move favicon into dist for production
+gulp.task("move-favicon", () => {
+  return gulp
+    .src(`${config.paths.src}/favicon.ico`)
+    .pipe(gulp.dest(config.paths.dist));
+});
+
+// Build scripts for production
+gulp.task("build-scripts", () => {
+  return gulp
+    .src(`${config.paths.src}/scripts/**/*.js`)
+    .pipe(babel({ presets: ["env"] }))
+    .pipe(concat("bundle.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest(`${config.paths.dist}/scripts`));
+});
+
+// Build styles for production
+gulp.task("build-styles", () => {
+  return gulp
+    .src(`${config.paths.src}/stylesheets/scss/main.sass`)
+    .pipe(sass())
+    .pipe(autoprefixer({ browsers: ["last 2 versions"], cascade: false }))
+    .pipe(cssnano())
+    .pipe(rename("bundle.css"))
+    .pipe(gulp.dest(`${config.paths.dist}/styles`));
+});
+
+// Moves index.html into dist and replaces all script and style refs with production bundles
+gulp.task("build-index", function() {
+  gulp
+    .src(`${config.paths.src}/index.html`)
+    .pipe(
+      htmlreplace({
+        css: "styles/bundle.css",
+        js: "scripts/bundle.js"
+      })
+    )
+    .pipe(gulp.dest(config.paths.dist));
 });
 
 // Compile + watch SASS, load browser
-gulp.task("serve", function(callback) {
+gulp.task("serve", callback => {
   runSequence(["sass", "browserSync", "watch"], callback);
+});
+
+// Clean dist + run Gulp tasks
+gulp.task("build", callback => {
+  runSequence(
+    "clean:dist",
+    "build-styles",
+    "build-scripts",
+    ["compress-images", "move-favicon", "move-fonts"],
+    "build-index",
+    callback
+  );
 });
